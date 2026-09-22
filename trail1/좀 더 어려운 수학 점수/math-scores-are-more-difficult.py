91be4c9f2f385bd02943a564a342cd0b89d4e5ae
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-09-23 07:14:49 +0900

    [Passed] 좀 더 어려운 수학 점수 (51ms, 16MB)

---
+++ trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+c, d = map(int, input().split())
+
+if a > c:
+    print('A')
+elif a == c:
+    if b > d:
+        print('A')
+    else:
+        print('B')
+else:
+    print('B')
