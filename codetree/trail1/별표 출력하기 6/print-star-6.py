5db07dc59dac5085a4f4d6f64aea1bbd14cb0995
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-09-25 15:11:12 +0900

    [Passed] 별표 출력하기 6 (48ms, 16MB)

---
+++ codetree/trail1/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+for i in range(N,0,-1):
+    for k in range(N-i):
+        print(" ",end=' ')
+
+    for j in range(2*i-1):
+        print("*",end=' ') 
+    print()
+
+for i in range(2,N+1):
+    for j in range(N-i):
+        print(" ",end=' ')
+
+    for k in range(2*i-1):
+        print("*",end=' ')
+    print()
+    
