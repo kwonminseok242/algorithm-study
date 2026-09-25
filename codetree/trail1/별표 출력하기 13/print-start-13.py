1502892e77eaebd63d48ea13caa38196c9392c0a
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-09-25 19:11:36 +0900

    [Passed] 별표 출력하기 13 (51ms, 16MB)

---
+++ codetree/trail1/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,14 @@
+N = int(input())
+cnt1 = 0
+cnt2 = N+1
+for i in range(1,N*2+1):
+    if i % 2 == 0: # 2, 4, 6, 8, 10
+        cnt1 += 1
+        for j in range(cnt1):
+            print("*", end=" ")
+        print()
+    else: # 1, 3, 5, 7,9
+        cnt2 -=1
+        for _ in range(cnt2):
+            print("*", end=" ")
+        print()
