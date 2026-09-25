303c525b819857ae1b1452ca848a222e735d047a
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-09-25 18:36:24 +0900

    [Passed] 체크 다이아몬드 (48ms, 16MB)

---
+++ codetree/trail1/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,22 @@
+N = int(input())
+# nn*
+# n*n*
+# *n*n*
+# n*n*n
+# nn*
+
+for i in range(1,N+1):
+    for _ in range(N-i):
+        print(" ",end="")
+    for _ in range(i):
+        print("*",end=" ")
+    print()
+
+for i in range(N-1,0,-1):
+    for _ in range(N-i):
+        print(" ",end="")
+    for _ in range(i):
+        print("*",end=" ")
+    
+    
+    print()
