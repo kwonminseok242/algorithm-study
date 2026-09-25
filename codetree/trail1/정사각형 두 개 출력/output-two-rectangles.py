5c77f8b0bd6cf2637f3372ae63c41f36b1ce40a6
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-09-25 11:02:21 +0900

    [Passed] 정사각형 두 개 출력 (49ms, 16MB)

---
+++ codetree/trail1/정사각형 두 개 출력/output-two-rectangles.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    for _ in range(N):
+        print("*",end="")
+    print()
+print()
+for _ in range(N):
+    for _ in range(N):
+        print("*",end="")
+    print()
